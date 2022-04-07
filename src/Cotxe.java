5b297c3e0b8f10430586cab962f3bd0404ea9f70
sergidl <sergidiazlopez3@gmail.com>
2022-04-07 15:32:49 +0200

    tabs

--- src/Cotxe.java
+++ src/Cotxe.java
@@ -1,36 +1,36 @@
 public class Cotxe extends Vehicle{
-    private String color, matricula;
-    public Cotxe(String brand, int wheels, String color, String matricula) {
-        super(brand, wheels);
-        this.setColor(color);
-        this.setMatricula(matricula);
-    }
+	private String color, matricula;
+	public Cotxe(String brand, int wheels, String color, String matricula) {
+		super(brand, wheels);
+		this.setColor(color);
+		this.setMatricula(matricula);
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-    public String getMatricula() {
-        return matricula;
-    }
+	public String getMatricula() {
+		return matricula;
+	}
 
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
 
-    @Override
-    void bikeLine() {
-        System.out.println("Cotxe "+getBrand()+" No pot anar pel carril bici");
-    }
+	@Override
+	void bikeLine() {
+		System.out.println("Cotxe "+getBrand()+" No pot anar pel carril bici");
+	}
 
-    @Override
-    void needITV() {
-        System.out.println("Cotxe "+getBrand()+" Si necesita fer ITV");
-    }
-    
-    
+	@Override
+	void needITV() {
+		System.out.println("Cotxe "+getBrand()+" Si necesita fer ITV");
+	}
+	
+	
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,33 +1,33 @@
 abstract class Vehicle {
-    abstract void bikeLine();
-    abstract void needITV();
-    private String brand;
-    private int wheels;
-    
-    public Vehicle(String brand, int wheels) {
-        this.setBrand(brand);
-        this.setWheels(wheels);
-    }
+	abstract void bikeLine();
+	abstract void needITV();
+	private String brand;
+	private int wheels;
+	
+	public Vehicle(String brand, int wheels) {
+		this.setBrand(brand);
+		this.setWheels(wheels);
+	}
 
-    public Vehicle(){
-        this(null, 0);
-    }
+	public Vehicle(){
+		this(null, 0);
+	}
 
-    public String getBrand() {
-        return brand;
-    }
+	public String getBrand() {
+		return brand;
+	}
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
 
-    public int getWheels() {
-        return wheels;
-    }
+	public int getWheels() {
+		return wheels;
+	}
 
-    public void setWheels(int wheels) {
-        this.wheels = wheels;
-    }
+	public void setWheels(int wheels) {
+		this.wheels = wheels;
+	}
 
 
 
